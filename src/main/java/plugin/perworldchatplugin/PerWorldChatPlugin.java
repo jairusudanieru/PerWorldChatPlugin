d246da6dc488d0eec3825087872a8dfa81ad5461
jairusu <jairusudanieru@gmail.com>
2023-07-02 13:46:36 +0800

    perworldchat

--- src/main/java/plugin/perworldchatplugin/PerWorldChatPlugin.java
+++ src/main/java/plugin/perworldchatplugin/PerWorldChatPlugin.java
@@ -1,6 +1,7 @@
 package plugin.perworldchatplugin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import plugin.perworldchatplugin.Commands.Pwcp;
@@ -21,16 +22,17 @@ public final class PerWorldChatPlugin extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
         checkAuthMePlugin();
-        Bukkit.getPluginCommand("pwcp").setExecutor(new Pwcp(this));
+        PluginCommand command = Bukkit.getPluginCommand("pwcp");
+        if (command != null) command.setExecutor(new Pwcp(this));
         Bukkit.getPluginManager().registerEvents(new ChangeWorld(this),this);
         Bukkit.getPluginManager().registerEvents(new PlayerChat(this),this);
         Bukkit.getPluginManager().registerEvents(new PlayerJoin(this),this);
         Bukkit.getPluginManager().registerEvents(new PlayerQuit(this),this);
-        Bukkit.getLogger().info("PerWorldChatPlugin successfully enabled!");
+        Bukkit.getLogger().info("[PerWorldChatPlugin] Plugin Successfully Enabled!");
     }
 
     @Override
     public void onDisable() {
-        Bukkit.getLogger().info("PerWorldChatPlugin successfully disabled!");
+        Bukkit.getLogger().info("[PerWorldChatPlugin] Plugin Successfully Disabled!");
     }
 }
